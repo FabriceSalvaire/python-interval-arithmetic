7b10f83df850fd8ff2ba4cc93fcf778477dc5e3e
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-11-11 20:42:33 +0100

    udpated setup

--- setup_data.py
+++ setup_data.py
@@ -1,18 +1,20 @@
 ####################################################################################################
 #
-# ArithmeticInterval - A Python Arithmetic Interval Module
+# IntervalArithmetic - An Interval Arithmetic Module
 # Copyright (C) 2015 Fabrice Salvaire
 #
-# This program is free software: you can redistribute it and/or modify it under the terms
-# of the GNU General Public License as published by the Free Software Foundation,
-# either version 3 of the License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program.
-# If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ####################################################################################################
 
@@ -20,38 +22,71 @@
 
 import os
 import sys
+import subprocess
+
+####################################################################################################
+
+def merge_include(src_lines, doc_path, included_rst_files=None):
+    if included_rst_files is None:
+        included_rst_files = {}
+    text = ''
+    for line in src_lines:
+        if line.startswith('.. include::'):
+            include_file_name = line.split('::')[-1].strip()
+            if include_file_name not in included_rst_files:
+                # print "include", include_file_name
+                with open(os.path.join(doc_path, include_file_name)) as f:
+                    included_rst_files[include_file_name] = True
+                    text += merge_include(f.readlines(), doc_path, included_rst_files)
+        else:
+            text += line
+    return text
 
 ####################################################################################################
 
 # Utility function to read the README file.
 # Used for the long_description.
-def read(file_name):
+def read_readme(file_name):
 
-    path = os.path.dirname(__file__)
-    if os.path.basename(path) == 'tools':
-        path = os.path.dirname(path)
-    absolut_file_name = os.path.join(path, file_name)
+    source_path = os.path.dirname(os.path.realpath(__file__))
+    if os.path.basename(source_path) == 'tools':
+        source_path = os.path.dirname(source_path)
+    elif 'build/bdist' in source_path:
+        source_path = source_path[:source_path.find('build/bdist')]
+    absolut_file_name = os.path.join(source_path, file_name)
+    doc_path = os.path.join(source_path, 'doc', 'sphinx', 'source')
 
-    if os.path.exists(absolut_file_name):
-        return open(absolut_file_name).read()
-    else:
-        sys.stderr.write("WARNING: README {} not found\n".format(absolut_file_name))
-        return ''
+    # Read and merge includes
+    with open(absolut_file_name) as f:
+        lines = f.readlines()
+    text = merge_include(lines, doc_path)
+
+    return text
 
 ####################################################################################################
 
-long_description = read('README.rst')
+if not __file__.endswith('conf.py'):
+    long_description = read_readme('README.txt')
+else:
+    long_description = ''
+
+####################################################################################################
 
 setup_dict = dict(
-    name='ArithmeticInterval',
+    name='IntervalArithmetic',
     version='0.2.0',
     author='Fabrice Salvaire',
     author_email='fabrice.salvaire@orange.fr',
-    description='A Python Arithmetic Interval Module',
-    license = "GPLv3+",
-    keywords = "arithmetic interval",
-    url='https://github.com/FabriceSalvaire/python-arithmetic-interval',
-    packages=['ArithmeticInterval'],
+    description='An interval arithmetic module',
+    license='GPLv3',
+    keywords='interval arithmetic',
+    url='https://github.com/FabriceSalvaire/python-interval-arithmetic',
+    scripts=[],
+    packages=[  # Fixme:
+        'IntervalArithmetic'
+    ],
+    # package_dir={'IntervalArithmetic': 'IntervalArithmetic'},
+    package_data={},
     long_description=long_description,
     # cf. http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -62,4 +97,5 @@ setup_dict = dict(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.4",
         ],
-    )
+    install_requires=[],
+)
